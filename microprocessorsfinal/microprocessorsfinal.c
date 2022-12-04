7b3d3f9a906e83885e8925d94ad779a279250fbc
vastqud <66101476+vastqud@users.noreply.github.com>
2022-12-04 02:24:06 -0500

    semicolons

--- microprocessorsfinal/microprocessorsfinal.c
+++ microprocessorsfinal/microprocessorsfinal.c
@@ -157,13 +157,13 @@ int main() {
 #pragma vector = TIMER0_A0_VECTOR
 __interrupt void TimerA(void) { //interrupt is triggered every 50ms
     if (enabled == 1) {
-        timer_counter++
+        timer_counter++;
         decimal = decimal + 0.05; //increase decimal precision by 50ms
     }
 
     if (timer_counter >= 20) {
-        num++ //increment whole number seconds
+        num++; //increment whole number seconds
         decimal = 0; //reset decimal
-        timer_counter = 0
+        timer_counter = 0;
     }
 }
